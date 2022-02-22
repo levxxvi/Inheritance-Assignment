535293d82063d96ded3d07042956e31e9dfb8db1
levxxvi <99051920+levxxvi@users.noreply.github.com>
2022-02-22 15:01:50 -0800

    GasPlanet done! :D

--- src/GasPlanet.java
+++ src/GasPlanet.java
@@ -1,19 +1,19 @@
 import java.awt.*;
 
 public class GasPlanet extends Planet{
-    boolean hasRings;
-    public Color color;
-    GasPlanet(int orbitTime, String destination, boolean hasRings, Color color) {
+    private boolean hasRings;
+    private Color color;
+    public GasPlanet(int orbitTime, String destination, boolean hasRings, Color color) {
         super(orbitTime, destination);
     }
-    static boolean isHasRings(){
+    public static boolean isHasRings(){
         return false;
     }
-    void setHasRings(boolean isHasRings){}
-    static Color getColor(){
+    public void setHasRings(boolean isHasRings){}
+    public static Color getColor(){
         return null;
     }
-    void setColor(Color getColor){}
+    public void setColor(Color getColor){}
     public String toString(){
         return null;
     }
