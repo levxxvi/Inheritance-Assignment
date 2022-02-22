9be5c79e443e023eba30961b28699a148aadb02f
levxxvi <99051920+levxxvi@users.noreply.github.com>
2022-02-22 14:02:54 -0800

    Initial commit

---
+++ src/Moon.java
@@ -0,0 +1,5 @@
+public class Moon {
+    int orbitTime;
+    boolean atmosphere;
+    String designation;
+}
---
+++ src/Planet.java
@@ -0,0 +1,10 @@
+import java.util.ArrayList;
+
+public class Planet {
+    int orbitTime;
+    String destination;
+    ArrayList<Moon> moons;
+    public static void main(String[] args) {
+
+    }
+}
